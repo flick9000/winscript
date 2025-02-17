adc1a82b4b34109a426bcde448e733646dbec572
flick9000 <137506210+flick9000@users.noreply.github.com>
2025-02-17 16:11:58 +0100

    Landing Page Animations

--- main.js
+++ main.js
@@ -33,3 +33,17 @@ openStartDialog.addEventListener("click", () => {
 closeStartDialog.addEventListener("click", () => {
   startDialog.close();
 })
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.3
+});
+
+document.querySelectorAll('.fade-in').forEach((element) => {
+  observer.observe(element);
+});
