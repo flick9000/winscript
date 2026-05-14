7f71f3b41a9ab3178f0860c2e26b5ab8b152ce34
Francesco <137506210+flick9000@users.noreply.github.com>
2026-05-14 21:28:10 +0200

    feat(i18n): add italian language (#157)
    * feat: add italian language
    
    * fix: missing translation for an item
    
    * fix: add missing translations for shortcuts
    
    * refactor: change wordings and TitleCase
    
    * fix(website): add missing translations for shortcuts

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   i18n: {
-    locales: ["en", "tr"],
+    locales: ["en", "tr", "it"],
     defaultLocale: "en",
   },
 });
