3b25fb6c4bd363738b37e91ccf8aac55593b6278
flick9000 <137506210+flick9000@users.noreply.github.com>
2026-05-16 14:25:37 +0200

    refactor: read output instead of temp files for updater checks

--- app/src/assets/js/main.js
+++ app/src/assets/js/main.js
@@ -121,10 +121,8 @@ async function alertForUpdates() {
 }
 
 async function isInstalled() {
-  const tmpDir = await tempDir();
-
   async function checkProcess() {
-    const processCheck = `([bool](Get-Process winscript-portable -ErrorAction SilentlyContinue)).ToString().ToLower() | Set-Content -Path $env:TEMP\\isPortable.txt;`;
+    const processCheck = `([bool](Get-Process winscript-portable -ErrorAction SilentlyContinue)).ToString().ToLower()`;
     const processShell = new Command("powershell", [
       "-NoProfile",
       "-WindowStyle",
@@ -132,41 +130,33 @@ async function isInstalled() {
       "-Command",
       processCheck,
     ]);
-    await processShell.execute();
+    const command = await processShell.execute();
+    return command.stdout.trim();
   }
 
-  async function checkPath() {
-    const pathCheck = `(Get-Process -Id (Get-CimInstance Win32_Process -Filter "ProcessId=$PID").ParentProcessId).Path | Set-Content -Path $env:TEMP\\winscriptPath.txt;`;
+  async function checkUninstaller() {
+    const pathCheck = `(Get-Process -Id (Get-CimInstance Win32_Process -Filter "ProcessId=$PID").ParentProcessId).Path`;
     const pathShell = new Command("powershell", ["-Command", pathCheck]);
-    await pathShell.execute();
+    const command = await pathShell.execute();
+    const uninstallExists = await exists(
+      await join(await dirname(command.stdout.trim()), "uninstall.exe"),
+    );
+    return uninstallExists;
   }
 
-  function cleanFiles() {
-    remove(tmpDir + "isPortable.txt");
-    remove(tmpDir + "winscriptPath.txt");
-  }
+  const isPortable = await checkProcess();
+  const uninstallExists = await checkUninstaller();
 
-  await checkProcess();
-  await checkPath();
+  console.log(isPortable, uninstallExists);
 
-  const isPortableBool = (await readTextFile(tmpDir + "isPortable.txt")).trim() === "true";
-
-  if (isPortableBool) {
-    cleanFiles();
+  if (isPortable) {
     alertForUpdates();
     return;
   }
 
-  const winscriptPath = (await readTextFile(tmpDir + "winscriptPath.txt")).toLowerCase();
-  const winscriptDir = await dirname(winscriptPath);
-  const uninstallPath = await join(winscriptDir, "uninstall.exe");
-  const uninstallExists = await exists(uninstallPath);
-
   if (uninstallExists) {
-    cleanFiles();
     checkForUpdates();
   } else {
-    cleanFiles();
     alertForUpdates();
   }
 }
