aee778bd121a072580a04215cfd5e18624131eb9
Francesco <137506210+flick9000@users.noreply.github.com>
2026-05-19 19:37:59 +0200

    feat(i18n): add russian localization (#165)
    * Add Russian localization (#164)
    
    Added Russian localization file
    
    * feat(website): add russian localization
    
    * feat(i18n): add russian as language
    
    * fix: typos and change wordings
    
    * fix: typos
    
    * fix: button padding
    
    ---------
    
    Co-authored-by: sanda1iki <41101916+qud0@users.noreply.github.com>

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   i18n: {
-    locales: ["en", "tr", "it"],
+    locales: ["en", "ru", "tr", "it"],
     defaultLocale: "en",
   },
 });
