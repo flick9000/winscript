6710d41746bd63307686450ca7ae93da7f3b4203
Francesco <137506210+flick9000@users.noreply.github.com>
2026-05-30 17:48:39 +0200

    feat(i18n): add spanish localization (#182)
    * feat(i18n): Spanish Translation (#181)
    
    * Add files via upload
    
    * Update 260529c_es.json
    
    * Update 260529c_es.json
    
    * Add messages for Winget and Chocolatey availability
    
    The two missing strings have been translated
    
    ---------
    
    * fix: path and rename
    
    * feat(i18n): add español as language
    
    * fix: missing periods and grammar
    
    ---------
    
    Co-authored-by: dracp <102802425+dracp@users.noreply.github.com>

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   i18n: {
-    locales: ["en", "ru", "tr", "it", "ko"],
+    locales: ["en", "ru", "tr", "it", "ko", "es"],
     defaultLocale: "en",
   },
 });
