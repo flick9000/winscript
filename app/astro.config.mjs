9773a6c5044a0ced0ccb72ab5f62126293b39107
Francesco <137506210+flick9000@users.noreply.github.com>
2026-05-31 23:03:02 +0200

    feat(i18n): add portoguese localization (#190)
    * feat(i18n): add portoguese localization
    
    * fix: TitleCase, infinitive and 3rd-person forms & technical terms

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   i18n: {
-    locales: ["en", "ru", "tr", "it", "ko", "es"],
+    locales: ["en", "ru", "tr", "it", "ko", "es", "pt"],
     defaultLocale: "en",
   },
 });
