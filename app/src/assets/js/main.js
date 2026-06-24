5266128fca83af633eb497d5e213492e7dc77b09
flick9000 <137506210+flick9000@users.noreply.github.com>
2026-06-24 23:26:56 +0200

    feat: update all installed apps using preferred package manager

--- app/src/assets/js/main.js
+++ app/src/assets/js/main.js
@@ -146,6 +146,17 @@ function applyMica() {
   }
 }
 
+async function hasWindowsTerminal() {
+  try {
+    const check = new Command("where", ["wt"]);
+    const output = await check.execute();
+    return output.code === 0;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
 applyMica();
 
 await loadLocale();
@@ -404,6 +415,51 @@ document.getElementById("searchBar").addEventListener("input", () => {
   });
 });
 
+// Update apps button
+const updateBtn = document.getElementById("updateBtn");
+const packageManager = document.getElementById("packageManager");
+const chocoUpdateCommand = "choco upgrade all -y";
+const wingetUpdateCommand =
+  "winget upgrade --all --include-unknown --silent --accept-source-agreements --accept-package-agreements";
+
+async function updateApps(packageUpdateCommand) {
+  const hasWt = await hasWindowsTerminal();
+  const args = hasWt
+    ? [
+        "/c",
+        "start",
+        "wt",
+        "powershell",
+        "-NoProfile",
+        "-NoLogo",
+        "-ExecutionPolicy",
+        "Bypass",
+        "-Command",
+        packageUpdateCommand,
+      ]
+    : [
+        "/c",
+        "start",
+        "powershell",
+        "-NoProfile",
+        "-NoLogo",
+        "-ExecutionPolicy",
+        "Bypass",
+        "-Command",
+        packageUpdateCommand,
+      ];
+
+  new Command("cmd", args).execute();
+}
+
+updateBtn.addEventListener("click", () => {
+  if (packageManager.value === "chocolatey") {
+    updateApps(chocoUpdateCommand);
+  } else if (packageManager.value === "winget") {
+    updateApps(wingetUpdateCommand);
+  }
+});
+
 // Copy to clipboard button
 document.getElementById("copyBtn").addEventListener("click", function () {
   var textContent = document.getElementById("code").innerText;
@@ -471,17 +527,6 @@ document.getElementById("runBtn").addEventListener("click", async function () {
     await mkdir(dirPath, { recursive: true });
 
     await writeTextFile(filePath, textContent);
-
-    async function hasWindowsTerminal() {
-      try {
-        const check = new Command("where", ["wt"]);
-        const output = await check.execute();
-        return output.code === 0;
-      } catch (error) {
-        console.error(error);
-        return false;
-      }
-    }
 
     const hasWt = await hasWindowsTerminal();
 
